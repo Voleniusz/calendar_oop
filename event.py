a8b89fe1cc37ec37cbcb0f4724b0ff8eb4238ce9
Dawid Hulbój <dawhulboj@gmail.com>
2022-05-14 11:40:12 +0200

    object represantion

--- event.py
+++ event.py
@@ -25,7 +25,7 @@ class Event:
             raise ValueError(f'Invalid date format, use DD-MM-YYYY HH:MM, {val}')
         if parsed_date < datetime.now() + timedelta(minutes=15):
             raise ValueError(f'Not enough time to organize a meeting: {parsed_date - datetime.now()}')
-        self._start_time = val
+        self._start_time = parsed_date
 
     @property
     def title(self):
@@ -52,9 +52,18 @@ class Event:
 
         self._duration = timedelta(minutes=value)
 
+    def __str__(self):
+        delta_time = self.start_time - datetime.now()
+        hours, rest = delta_time.seconds // (60 * 60), delta_time.seconds % (60 * 60)
+        minutes, seconds = rest // 60, rest % 60
+        days = f'{delta_time.days} days' if delta_time.days > 0 else ''
+        seconds = f'0{seconds}'[-2:]
+        minutes = f'0{minutes}'[-2:]
+        return f'{self.title}, time to event: {days}{hours}:{minutes}:{seconds}'
 
-e = Event(42, '', '14-05-2022 11:30', 11, '', '')
-print(e.start_time)
+
+e = Event(42, '', '14-05-2022 12:50', 11, '', '')
+print(e)
 
 
 
